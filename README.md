c4957f8eecf935f252243f0058017d9a6675674e
mattcdrake <drake.mattc@gmail.com>
2018-01-05 12:06:34 -0500

    submission ready

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ud036_StarterCode
-Source code for a Movie Trailer website.
+
+## Goal
+
+Generates a website that shows the posters and trailers of my favorite movies - part of the first FSND project. 
+
+## Running 
+
+On Linux, you can get this to run by cloning the repo and then running `entertainment_center.py` which should open the generated web site in your given browser.
---
+++ entertainment_center.py
@@ -0,0 +1,30 @@
+import media
+import fresh_tomatoes
+
+# Creates Movie objects for each of my favorite movies
+the_fountain = media.Movie("The Fountain", "Hugh Jackman tries to find "
+                           "the cure for cancer.",
+                           "https://images-na.ssl-images-amazon."
+                           "com/images/M/MV5BMTU5OTczMTcxMV5BMl5BanBnXkFtZTc"
+                           "wNDg3MTEzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                           "https://www.youtube.com/watch?v=dAuxryJ6pv8")
+
+eternal_sunshine = media.Movie("Eternal Sunshine of the Spotless Mind",
+                               "Jim Carrey goes through a mind bending "
+                               "journey while trying to erase his "
+                               "ex-girlfriend from his memory",
+                               "https://i.ytimg.com/vi/NnqPCBAp2fM/"
+                               "movieposter.jpg",
+                               "https://www.youtube.com/watch?v=GBEke6JixyE")
+
+drive = media.Movie("Drive", "Ryan Gosling is a Hollywood stuntman that "
+                    "also acts as a getaway driver.",
+                    "http://t3.gstatic.com/images?"
+                    "q=tbn:ANd9GcR_8gJBIwyEVuJsJRU0JQmm7ZJhZ3"
+                    "_uS8dpWW4Xd2_Kgv8wIPx4",
+                    "https://www.youtube.com/watch?v=KBiOF3y1W0Y")
+
+movies = [the_fountain, eternal_sunshine, drive]
+
+# Calls provided function to generate a web page
+fresh_tomatoes.open_movies_page(movies)
